9b753fd60c76e61d91c070d197005650c479b6de
Oluseye Lamina <oluseyelam@gmail.com>
2024-10-25 22:34:03 -0700

    my first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+ // This is my first javascript code
+// console.log("Hello World");
+//let name = 'Ola';
+//console.log(name);
+//const interestRate = 33;
+//interestRate = 2.9
+//console.log(interestRate)
+//interestRate = 'Bola';
+//console.log(interestRate)
+ 
+/** let person = {
+    name:'Segun',
+    age: 30
+};
+//person.age= 40;
+//person.name='Chuks'
+person['name'] = 'Segun'
+console.log(person.name)**/
+
+let selectedColours = ['red', 'blue'];
+selectedColours[2] = 'white';
+selectedColours[3] = 5;
+selectedColours[4] = 7;
+console.log(selectedColours.length );
